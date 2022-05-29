3a095e9852141465b8a651d44b9f5e10c4399a2c
Sebastian Kroczek <me@xbug.de>
2022-05-29 17:50:47 +0200

    Implement kanban-board

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container p-8 mx-auto">
+    <input
+        v-model="newTask"
+        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded-lg border-gray-200 appearance-none focus:outline-none focus:shadow-outline"
+        type="text"
+        placeholder="Add a task"
+        @keyup.enter="addTask"
+    >
+    <div class="grid grid-cols-3 gap-4 mt-4">
+      <div class="px-3 py-3 bg-gray-100 rounded-lg">
+        <p class="text-sm font-semibold tracking-wide text-gray-700">
+          To-Do
+        </p>
+        <KanbanColumn :list="todo" @click-close="removeTask($event, todo)" />
+      </div>
+      <div class="px-3 py-3 bg-gray-100 rounded-lg">
+        <p class="text-sm font-semibold tracking-wide text-gray-700">
+          Doing
+        </p>
+        <KanbanColumn :list="doing" @click-close="removeTask($event, doing)" />
+      </div>
+      <div class="px-3 py-3 bg-gray-100 rounded-lg">
+        <p class="text-sm font-semibold tracking-wide text-gray-700">
+          Done
+        </p>
+        <KanbanColumn :list="done" @click-close="removeTask($event, done)" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import KanbanColumn from "@/components/KanbanColumn.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    KanbanColumn
+  },
+  data() {
+    return {
+      newTask: '',
+      todo: [
+        {name: 'Todo 1'},
+        {name: 'Todo 2'},
+      ],
+      doing: [],
+      done: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask) {
+        this.todo.push({name: this.newTask});
+        this.newTask = '';
+      }
+    },
+    removeTask(task, list) {
+      const index = list.indexOf(task);
+      if (index !== -1) {
+        list.splice(index, 1);
+      }
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,33 @@
+<template>
+  <draggable :v-model="list"  class="h-full rounded-lg" :list="list" item-key="id" group="task" animation="200">
+    <template #item="{element}">
+      <div
+          class="flex items-start justify-between px-3 pt-3 pb-5 mt-3 bg-white border border-white rounded shadow"
+      >
+        <p>{{ element.name }}</p>
+        <div class="cursor-pointer" @click="$emit('clickClose', element)">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+          </svg>
+        </div>
+      </div>
+    </template>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    draggable
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ["clickClose"]
+}
+</script>
